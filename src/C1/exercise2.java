816784ddd4e3fb4632941f39b3195d59909c0170
LY Kang <ada890312@gmail.com>
2024-09-02 11:01:20 +0800

    commit 0902

---
+++ src/C1/exercise2.java
@@ -0,0 +1,27 @@
+package C1;
+
+import java.util.Scanner;
+
+public class exercise2{
+	public static void main(String[] args) {
+		double m,n;
+		Scanner scan=new Scanner(System.in);
+		System.out.println("費氏係數運算");
+		System.out.println("輸入整數n:");
+		n=scan.nextDouble();
+		System.out.println(n+"的費氏係數是"+fib(n));
+		
+	}
+	public static double fib(double n) {
+		double ans;
+		if (n==0) {
+			ans=0;
+		}else if(n==1) {
+			ans=1;
+		}else {
+			ans=fib(n-1)+fib(n-2);
+		}return ans;
+	}
+}
+
+
